39e2e55387dfb33e26abab56b733a63b5b71acc5
naveen-sekhar <naveensekhar06@gmail.com>
2025-07-08 09:28:22 +0530

    basics

---
+++ src/function_basics.java
@@ -0,0 +1,9 @@
+// Printing statement using functions
+public class function_basics {
+    static void print(int num){
+        System.out.println("The number you passed is "+num);
+    }
+    public static void main(String[] args) {
+        print(20);
+    }
+}
