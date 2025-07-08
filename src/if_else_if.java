ccfd7cf88ae8a335f4c745d88f69b532b4b37709
naveen-sekhar <naveensekhar06@gmail.com>
2025-07-08 08:38:07 +0530

    if_else_if

---
+++ src/if_else_if.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class if_else_if {
+    public static void main(String[] args) {
+        Scanner SR =  new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        float num = SR.nextFloat();
+        if (num%2==0 && num!=0){
+            System.out.println("The number is even");
+        } else if (num%2==1) {
+            System.out.println("The number is odd");
+        }
+        else{
+            System.out.println("Neither Even nor Odd");
+        }
+    }
+}
